1d06a7ca2d356aea7045fb65bb5dd7a269bd396e
devToGit <nnos@mail.ru>
2020-01-22 11:24:44 +0300

    8. Учебный проект: революция или эволюция? (Часть 1)

--- src/components/task.js
+++ src/components/task.js
@@ -1,6 +1,5 @@
-
 import {MonthNames} from '../const.js';
-import {formatTime} from '../utils.js';
+import {createElement, formatTime} from '../utils.js';
 
 const createHashtagsMarkup = (hashtags) => {
   return hashtags.map((hashtag) => {
@@ -15,7 +14,7 @@ const createHashtagsMarkup = (hashtags) => {
   .join(`\n`);
 };
 
-export const createTaskTemplate = (task) => {
+const createTaskTemplate = (task) => {
   const {description, tags, dueDate, color, repeatingDays} = task;
   const isExpired = dueDate instanceof Date && dueDate < Date.now();
   const isDateShowing = !!dueDate;
@@ -75,3 +74,27 @@ export const createTaskTemplate = (task) => {
       </article>`
   );
 };
+
+export default class Task {
+  constructor(task) {
+    this._task = task;
+
+    this._element = null;
+  }
+
+  getTemplate() {
+    return createTaskTemplate(this._task);
+  }
+
+  getElement() {
+    if (!this._element) {
+      this._element = createElement(this.getTemplate());
+    }
+
+    return this._element;
+  }
+
+  removeElement() {
+    this._element = null;
+  }
+}
